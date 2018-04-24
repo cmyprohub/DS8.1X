3838149c1b9f83dfd1f6ab2e29b0c44612e8330d
cmyprohub <myproaicloud@gmail.com>
2018-04-24 15:40:15 -0700

    Updated Apr 24 2018

---
+++ IceCreamCones.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 24 15:19:27 2018
+
+@author: Suma
+"""
+from datascience import *
+import numpy as np
+
+%matplotlib inline
+import matplotlib.pyplot as plots
+plots.style.use('fivethirtyeight')
+
+all_cones = Table.read_table('cones.csv')
+cones = all_cones.drop('Color').exclude(5)
+cones
+
+#Grouping by one column to get total counts
+cones.group('Flavor')
+
+#returns price list
+cones.group('Flavor', list)
+
+cones.group('Flavor', len)
+
+cones.group('Flavor', min)
+
+min(cones.where('Flavor', 'chocolate').column('Price'))
+
+cones.group('Flavor', np.average)
+
+#Function
+def data_range(x):
+    return max(x) - min(x)
+
+cones.group('Flavor', data_range)
+
+#Cross Classification
+all_cones
+all_cones.group('Flavor')
+
+all_cones.group(['Flavor', 'Color'])
+
+all_cones.group(['Flavor', 'Color'], max)
+
+
+
+
+
